9980691ca9b5c1e9dd03f7e7f41d4846d6941c11
tiffanypaulino <56327590+tiffanypaulino@users.noreply.github.com>
2020-03-05 19:35:19 -0800

    Update 01-larger-or-smaller.js

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -1,22 +1,12 @@
-// Create an application that accepts two integers within two separate prompts. 
-// Then, display only the larger of the two within the browser window. 
-// Don’t forget to handle the fact that the two could be equal.
-// https://www.ntu.edu.sg/home/ehchua/programming/webprogramming/JavaScript_Introduction.html
+let firstNum = prompt("Enter a number.");
+let secNum = prompt("Enter another number.");
 
-let num1, num2;
-    num1 = window.prompt("Input the first integer", 0);
-    num2 = window.prompt("Input the second integer", 0);
-    
-    // function integer(num1,num2) {
+if (firstNum > secNum) { 
+    window.document.write(firstNum); 
 
-        // var isInteger = Number.isInteger(num1) && Number.isInteger(num2);
-    
-        if(num1 < num2)
-            document.write(num2 + " is larger.");
-    
-        else if(num1 > num2)
-            document.write(num1 + " is larger.");
-        else if(num1 === num2)
-            document.write("The numbers are equal.")
-    // }
-    integer(-1,-1);
+} else if (secNum > firstNum) { 
+    window.document.write(secNum);
+
+} else if (firstNum === secNum) {
+    window.document.write("The numbers are equal.");
+}
